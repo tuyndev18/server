13c9ec93468f18f698cb08d7103a15a6d1bcbcbd
Tuyen Pham <tuyen197.it@gmail.com>
2022-06-15 10:14:25 +0700

    deploy v4.3

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const server = createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'https://f8-dev.vercel.app',
+    origin: 'https://f8-vn.vercel.app',
     methods: ['GET', 'POST'],
   },
 });
@@ -24,7 +24,7 @@ const io = new Server(server, {
 ConnectDB();
 
 app.use(express.json());
-app.use(cors({ origin: 'https://f8-dev.vercel.app', credentials: true }));
+app.use(cors({ origin: 'https://f8-vn.vercel.app', credentials: true }));
 app.use(cookieParser());
 
 socket(io);
