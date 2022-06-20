faa08c6938b4051e63d605dce65e0cba6e1f979f
Tuyen Pham <tuyen197.it@gmail.com>
2022-06-20 14:13:12 +0700

    make it better

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ const server = createServer(app);
 ConnectDB();
 
 app.use(express.json());
-app.use(cors({ origin: "*", credentials: true }));
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
 app.use(cookieParser());
 
 app.use("/public", express.static("public"));
